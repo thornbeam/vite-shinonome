bfa679cac0adf7be72406e5f174c8bcc1f21d5c3
thornbeam <thornbeam@posteo.eu>
2024-03-01 19:03:46 +0100

    fix(component): header - enhance rwd

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,38 +1,41 @@
-import { Link, NavLink } from "react-router-dom";
-import { useState } from "react";
+import { Link, NavLink } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function Component() {
-  const SharedClass = "fixed w-full px-3 uppercase";
+  const SharedClass = 'fixed w-full px-3 uppercase';
   const [isShown, setIsShown] = useState(false);
+
+  function handleClick() {
+    setIsShown(!isShown);
+  }
 
   return (
     <>
       <div
+        onMouseEnter={() => {
+          setIsShown(true);
+        }}
+        onMouseLeave={() => {
+          setIsShown(false);
+        }}
         className={`${SharedClass} h-[52px] z-20 grid grid-cols-2 sm:grid-cols-3 items-center`}
       >
         <div className="col-span-1">
-          <Link to="/">
-            Logo
-          </Link>
+          <Link to="/">Logo</Link>
         </div>
         <div className="col-span-1 h-full">
-          <div
-            onMouseEnter={() => {
-              setIsShown(true);
-            }}
-            onMouseLeave={() => {
-              setIsShown(false);
-            }}
-            className="h-full flex items-center justify-end sm:justify-center group"
-          >
-            <Hamburger isShown={isShown} />
-            <span className="ml-2">Menu</span>
+          <div className="h-full flex items-center justify-end sm:justify-center group">
+            <button
+              className="w-6 h-full"
+              onClick={handleClick}
+              onKeyDown={handleClick}
+            >
+              <Hamburger isShown={isShown} />
+            </button>
           </div>
         </div>
         <div className="col-span-1 hidden sm:flex justify-end">
-          <Link to="/news">
-            News
-          </Link>
+          <Link to="/news">News</Link>
         </div>
       </div>
 
@@ -43,17 +46,17 @@ export default function Component() {
         onMouseLeave={() => {
           setIsShown(false);
         }}
-        className={`${SharedClass} top-[52px] z-10 grid grid-cols-8 pb-4 ${isShown ? "translate-y-0 opacity-100" : "-translate-y-full opacity-0"} transition-all duration-300`}
+        className={`${SharedClass} top-[52px] z-10 grid grid-cols-8 pb-4 ${isShown ? 'translate-y-0 opacity-100' : '-translate-y-full opacity-0'} transition-all duration-300`}
       >
         <div className="col-span-8 sm:col-span-6 lg:col-span-4 sm:col-start-3 lg:col-start-5 grid grid-rows-4 sm:grid-rows-3 grid-flow-col gap-x-4 text-start w-full">
-          <NavLink to="/project">Project</NavLink>
-          <NavLink to="/category/project/category01">Category 01</NavLink>
-          <NavLink to="/category/project/category02">Category 02</NavLink>
-          <NavLink to="/page01">Page 01</NavLink>
-          <NavLink to="/page02">Page 02</NavLink>
-          <NavLink to="/team">Team</NavLink>
-          <NavLink to="/news">News</NavLink>
-          <NavLink to="/contact">Contact</NavLink>
+          <NavLink onClick={handleClick} to="/project">Project</NavLink>
+          <NavLink onClick={handleClick} to="/category/project/category01">Category 01</NavLink>
+          <NavLink onClick={handleClick} to="/category/project/category02">Category 02</NavLink>
+          <NavLink onClick={handleClick} to="/page01">Page 01</NavLink>
+          <NavLink onClick={handleClick} to="/page02">Page 02</NavLink>
+          <NavLink onClick={handleClick} to="/team">Team</NavLink>
+          <NavLink onClick={handleClick} to="/news">News</NavLink>
+          <NavLink onClick={handleClick} to="/contact">Contact</NavLink>
         </div>
       </div>
     </>
@@ -61,21 +64,19 @@ export default function Component() {
 }
 
 function Hamburger({ isShown }: { isShown: boolean }) {
-  const transition = "transition-all duration-300";
-  const BarClass = "h-px bg-black " + transition;
+  const transition = 'transition-all duration-300';
+  const BarClass = 'absolute top-1/2 left-0 h-[1.5px] bg-black ' + transition;
   return (
     <>
-      <div
-        className={`flex flex-col justify-between w-4 ${transition} ${isShown ? "h-3" : "h-2.5"}`}
-      >
+      <div className="relative w-full h-full">
         <span
-          className={`${BarClass} w-full ${isShown ? "rotate-45" : "rotate-0"} origin-top-left`}
+          className={`${BarClass} ${isShown ? 'rotate-45 w-5 translate-y-0' : 'rotate-0 w-6 -translate-y-1.5'}`}
         ></span>
         <span
-          className={`${BarClass} ${isShown ? "w-0" : "w-full"} origin-center`}
+          className={`${BarClass} ${isShown ? 'w-0' : 'w-6'} origin-center`}
         ></span>
         <span
-          className={`${BarClass} w-full origin-bottom-left ${isShown ? "-rotate-45" : "rotate-0"}`}
+          className={`${BarClass} ${isShown ? '-rotate-45 w-5 translate-y-0' : 'rotate-0 w-6 translate-y-1.5'}`}
         ></span>
       </div>
     </>
